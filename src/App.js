383f27f17a1b6d5ad84bc87a44464da66c9f6bf9
Duncan Leung <leungd@gmail.com>
2017-08-11 08:40:59 -0700

    Set Up Prettier and styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import logo from "./logo.svg";
+
+const AppStyles = styled.div`text-align: center;`;
+
+const HeaderStyles = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Intro = styled.p`font-size: large;`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppStyles>
+        <HeaderStyles>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </HeaderStyles>
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </Intro>
+      </AppStyles>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+}`;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
