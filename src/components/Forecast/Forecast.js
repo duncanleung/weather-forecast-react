147d616f20d69bf9818f68b7842485c2322fa15a
Duncan L <leungd@gmail.com>
2017-08-18 09:32:03 -0700

    Clean Up Forecast.js Remove console.logs

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-import styled from "styled-components";
 
 import Location from "./Location";
 
